c0592abd457e98fe98657d914a4ca08a740a06e5
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-04-15 12:35:29 +0530

    Added gdb macros

---
+++ gdb/init.gdb
@@ -0,0 +1,27 @@
+python
+
+import sys
+import os
+import glob
+
+gdb_dir = os.path.join(os.environ['HOME'], 'Gdb')
+python_dir = os.path.join(gdb_dir, 'python')
+printers_dir = os.path.join(gdb_dir, 'printers')
+
+gdb_files = glob.glob(os.path.join(gdb_dir, 'gdb/*.gdb'))
+for f in gdb_files:
+  print 'sourcing ' + f
+  gdb.execute('source ' + f)
+
+sys.path.insert (0, python_dir)
+import preferences
+preferences.init()
+
+sys.path.insert (0, printers_dir)
+from libstdcxx.v6 import libstdcxx
+libstdcxx.register_libstdcxx_printers(None)
+
+from qt.v4 import qt
+qt.register_qt_printers(None)
+
+end
--- perl-scripts/GC.pl
+++ perl-scripts/GC.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Text::Tabs;
 use File::Find;
 use Getopt::Long qw(:config no_ignore_case);
 
@@ -43,8 +44,9 @@ foreach (@$files) {
   open FILE, '<', $fileName or die "Cannot open file $!";
   open FILESAVE, ">" . $tmpFileName;
   while (<FILE>) {
-    s/^\s*(public|private|protected)\s*slots\s*:\s*/$1 slots:\n    /;
-    print FILESAVE $_;
+    s/^\s*(public|private|protected)\s*slots\s*:\s*/\n$1 slots:\n    /;
+    my $line = expand ($_);
+    print FILESAVE $line;
   }
   close (FILE);
   rename ($tmpFileName, $fileName) or print "Cannot rename tmp file $tmpFileName";
