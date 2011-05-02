f5a67aeb6607ade9387bc2e6e7b78b76d3ce28a0
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-05-02 16:16:07 +0530

    Gvim.pl file updated. Added couple more command line flags -v, -p to launch the gvimrc file and gvim.pl files respectively. On windows ,l command in windows opens up gvimrc-local file in $VIM directory

--- perl-scripts/gvim.pl
+++ perl-scripts/gvim.pl
@@ -5,15 +5,23 @@ use Win32::Registry;
 use Win32::TieRegistry;
 use Win32::Process;
 use Win32;
+use File::Spec;
+use File::Basename;
 use Getopt::Long qw(:config no_ignore_case);
 
-my %options =();
 my $files;
 my $vimOptions;
 my $server='default';
 my $lineNum=1;
 my $columnNum=1;
 my $readLocalSource;
+my $editPerlFile;
+my $editVimrcFile;
+my $vcSolutionName;
+
+my %solutionServerMap =  (
+  'GeneroStudio' => 'gst',
+);
 
 my $opts;
 
@@ -22,11 +30,18 @@ my $result = GetOptions (
 	'vimoptions|v=s@' => \$vimOptions,
 	'server|s=s' => \$server,
 	'line|l=i' => \$lineNum,
-	'col|c=i' => \$columnNum
+	'col|c=i' => \$columnNum,
+  'perl|p' => \$editPerlFile,
+  'vimrc|v' => \$editVimrcFile,
+  'vcproj=s' => \$vcSolutionName
 );
-	        
+
 my $reg = $Registry->Open ("LMachine/SOFTWARE/Vim/Gvim", {Access => KEY_READ(), Delimiter => '/'})
 	or die "Can't open HKEY_LOCAL_MACHINE key: $^E\n";
+
+my $gvim_exe = $reg->{'/path'};
+my $vim_dir = dirname(dirname($gvim_exe));
+my $gvimrc_file = File::Spec->catfile("$vim_dir", "_gvimrc");
 
 sub ErrorReport {
 	print Win32::FormatMessage (Win32::GetLastError());
@@ -35,8 +50,23 @@ sub ErrorReport {
 my $vimargs;
 push (@$vimargs, 'gvim');
 push (@$vimargs, '--servername');
+
+if ($vcSolutionName) {
+  $vcSolutionName =~ s/\.(sln|vcproj)$//;
+  if (exists($solutionServerMap{$vcSolutionName})) {
+    $server = $solutionServerMap{$vcSolutionName};
+  }
+}
+
 push (@$vimargs, $server);
 
+if ($editPerlFile) {
+  @$files = ();
+  push (@$files, __FILE__);
+} elsif ($editVimrcFile) {
+  @$files = ();
+  push (@$files, $gvimrc_file);
+}
 
 if ($files) {
 	push (@$vimargs, '--remote-tab-silent');
@@ -44,13 +74,18 @@ if ($files) {
 	foreach (@$files) {
 		push (@$vimargs, "\"$_\"");
 	}
+} elsif (@ARGV) {
+  push (@$vimargs, '--remote-tab-silent');
+  foreach (@ARGV) {
+		push (@$vimargs, "\"$_\"");
+  }
 }
 
 my $vimargs_string = join (' ', @$vimargs);
 
-my $ProcessObj;
+my $processObj;
 
-Win32::Process::Create ($ProcessObj, $reg->{'/path'}, $vimargs_string, 0,
+Win32::Process::Create ($processObj, $reg->{'/path'}, $vimargs_string, 0,
 	NORMAL_PRIORITY_CLASS, ".") || die ErrorReport();
 
-$ProcessObj->Resume();
+$processObj->Resume();
