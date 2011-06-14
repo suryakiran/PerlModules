efc0e00287544c714d34fcea2f0cc7aa1ee3a688
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-14 12:09:16 +0530

    Modifying GVim.pm

--- GVim.pm
+++ GVim.pm
@@ -1,4 +1,9 @@
 package GVim;
+use base qw 'Class::Accessor::Fast';
+
+__PACKAGE__->mk_ro_accessors(qw{
+    exe rc rc_local server_list
+});
 
 my %moduleMap = (
   'MSWin32' => 'Win32'
@@ -6,6 +11,23 @@ my %moduleMap = (
 
 my $module = $moduleMap{$OSNAME} || 'Linux';
 
-
 require ("GVim/${module}.pm");
 our @ISA = ("GVim::${module}");
+
+sub new {
+  my ($class, %params) = @_;
+
+  my $self = bless {
+    exe         => '',
+    rc          => '',
+    rc_local    => '',
+    server_list => []
+  }, $class;
+
+  $self->{exe} = $self->gvimExe();
+  $self->{rc} = $self->gvimrcFile();
+  $self->{rc_local} = $self->gvimrcLocalFile();
+  $self->{server_list} = $self->serverList();
+
+  return $self;
+}
